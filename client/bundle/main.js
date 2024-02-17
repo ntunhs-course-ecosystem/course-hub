8bbf1a0c46e3ea51a9e35c15c1714d3cc8b4853a
chinlinlee <a5566qq2581@gmail.com>
2024-02-17 19:10:24 +0800

    chore: add alpineJS

--- client/bundle/main.js
+++ client/bundle/main.js
@@ -1,1 +1,16 @@
 import "htmx.org";
+import Alpine from "alpinejs";
+
+
+window.Alpine = Alpine;
+
+// Start Alpine when the page is ready.
+window.addEventListener("DOMContentLoaded", (event) => {
+    Alpine.start();
+});
+
+
+// Restart Alpine when the DOM is altered by HTMX.
+document.body.addEventListener("htmx:afterSwap", () => {
+    Alpine.start();
+});
