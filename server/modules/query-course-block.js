0081a75ab333e3354888b20a28aa7937652cae33
chinlinlee <a5566qq2581@gmail.com>
2024-02-19 15:17:22 +0800

    feat: add infinite scroll for query course block

--- server/modules/query-course-block.js
+++ server/modules/query-course-block.js
@@ -8,6 +8,10 @@ const data = JSON.parse(fs.readFileSync(
     )
 ));
 
+async function sleep(ms) {
+    return new Promise(r=> setTimeout(r, ms));
+}
+
 /**
  * 
  * @param {import("fastify").FastifyInstance} fastify 
@@ -15,13 +19,23 @@ const data = JSON.parse(fs.readFileSync(
  */
 export default async function (fastify, opts) {
     fastify.get("/query-course-block", async (request, reply) => {
+        let page = request.query.page ? ++request.query.page : 1;
+
+        let returnData;
+        returnData = data.slice((page-1)*10, page*10);
+
+        if (returnData.length === 0) return reply.status(204).send();
+
+        await sleep(1000);
+
         return reply.view("components/layout", {
             page: {
                 file: "../query-course-block",
                 data: {
                     courses: [
-                        ...data.slice(0, 10)
-                    ]
+                        ...returnData
+                    ],
+                    page: page
                 }
             }
         });
