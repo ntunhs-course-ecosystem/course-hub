aadc0a589d1d2f219ce6faef82ae2982b1e0f2e0
chinlinlee <a5566qq2581@gmail.com>
2024-02-23 15:24:45 +0800

    feat: 基本課程查詢 layout

---
+++ server/modules/course-hub/routes/view.js
@@ -0,0 +1,10 @@
+export default async function (fastify, opts) {
+    fastify.get("/", async (request, reply) => {
+        return reply.view("components/layout", {
+            page: {
+                file: "../course-hub",
+                data: {}
+            }
+        });
+    });
+}
